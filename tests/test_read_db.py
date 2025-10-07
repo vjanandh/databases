bcac43cd30a85de351eae62f65f7f3076e63bf29
Vijay <150015592+vjanandh@users.noreply.github.com>
2025-10-07 12:43:50 +0100

    Update test_read_db.py

--- tests/test_read_db.py
+++ tests/test_read_db.py
@@ -6,6 +6,29 @@ con = sql.connect("tutorial.db")
 # Create a cursor to execute SQL statements and fetch results
 cur = con.cursor()
 
-# Print all rows iteratively
-for row in cur.execute("SELECT year, title, score FROM movies ORDER BY year"):
-    print(row)
+# Check if the table already exists or else create one
+res = cur.execute("SELECT name FROM sqlite_master")
+if res.fetchone() is None:
+    # Print all rows iteratively
+    for row in cur.execute("SELECT year, title, score FROM movies ORDER BY year"):
+        print(row)
+else:
+    # Create a table and specify the columns
+    cur.execute("CREATE TABLE movies(title, year, score)")
+    
+    # Check if the table has been created by fetching its name from the schema table of the database
+    res = cur.execute("SELECT name FROM sqlite_master")
+    print(res.fetchone())
+    
+    # Insert data into the table
+    cur.execute("""
+        INSERT INTO movies VALUES
+                ('Monty Python and the Holy Grail', 1975, 8.2),
+                ('And Now for Something Completely Different', 1971, 7.5)
+    """)
+    con.commit() # Commit changes to the database
+
+    # Fetch the existing data from the table
+    res = cur.execute("SELECT * from movies")
+    print(res.fetchall())
+con.close()
